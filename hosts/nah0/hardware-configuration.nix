a5e76ad34c683a1d1f898d26a1c0ad43bf236967
Hung-I Wang <whygowe@gmail.com>
2022-08-02 13:46:21 +0800

    style: nix format

--- hosts/nah0/hardware-configuration.nix
+++ hosts/nah0/hardware-configuration.nix
@@ -2,7 +2,8 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    [
+      (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" ];
@@ -11,12 +12,14 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/cd8bc5c4-ba44-4065-9d2d-8317356b1ebc";
+    {
+      device = "/dev/disk/by-uuid/cd8bc5c4-ba44-4065-9d2d-8317356b1ebc";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/94E8-7477";
+    {
+      device = "/dev/disk/by-uuid/94E8-7477";
       fsType = "vfat";
     };
 
