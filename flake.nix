140e81735031ace8f1696f75efaf34a0e045e365
Hung-I Wang <whygowe@gmail.com>
2022-08-03 21:09:00 +0800

    flake: refactor with genAttrs

--- flake.nix
+++ flake.nix
@@ -9,24 +9,12 @@
           system = "x86_64-linux";
         };
       };
-
-      nah0 = {
-        deployment = {
-          targetHost = "nah0.lotust.xyz";
-          # targetPort = 22;
-          # targetUser = "root";
-        };
-        imports = [ ./hosts/nah0 ];
-        # boot.isContainer = true;
-        # time.timeZone = "America/Los_Angeles";
-      };
-
-      air0 = {
-        deployment = {
-          targetHost = "air0.lotust.xyz";
-        };
-        imports = [ ./hosts/air0 ];
-      };
-    };
+    } // inputs.nixpkgs.lib.genAttrs [ "nah0" "air0" ] (name: {
+      deployment =
+        {
+          targetHost = "${name}.lotust.xyz";
+        }
+          imports = [ ./hosts/${name} ];
+    });
   };
 }
