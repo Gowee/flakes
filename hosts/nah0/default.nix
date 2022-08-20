ebfd946f5142dd3a2d1d283a0e46fe0cfaab095c
Hung-I Wang <whygowe@gmail.com>
2022-08-20 13:13:08 +0800

    nah0: disable mongodb

--- hosts/nah0/default.nix
+++ hosts/nah0/default.nix
@@ -3,7 +3,7 @@
   imports = with specialArgs; [
     ./configuration.nix
     ./gateway.nix
-    ./mongodb
+    # ./mongodb
     self.nixosModules.telegraf
     self.nixosModules.shadowsocks
     # ./influxdb2.nix
